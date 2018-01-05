70ac556f2429e853307e4b785f43cc6328010df7
rajagopalparimi <rajparimi99@gmail.com>
2018-01-04 19:28:00 -0800

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CsharpPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x;
+            int y;
+            int result;
+            Console.Write("\n Enter the first number to be added: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n Enter the second number to be added: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            result = x + y;
+            Console.Write("\n The sum of two numbers is: " + result);
+            Console.ReadLine();
+        }
+    }
+}
